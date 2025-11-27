8cf1ca28e18c6545866c6dfa914e452312582336
lailaporter55 <lmp3003@rit.edu>
2025-11-27 10:10:10 -0500

    helper

---
+++ client/helper.js
@@ -0,0 +1,32 @@
+const handleError =(messages) => {
+    document.getElementById('errorMessage').textContent = message;
+}
+
+  /* Sends post requests to the server using fetch. Will look for various
+     entries in the response JSON object, and will handle them appropriately.
+  */
+     const sendPost = async (url, data, handler) => {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        });
+    
+        const result = await response.json();
+        document.getElementById('domoMessage').classList.add('hidden');
+      
+        if(result.redirect) {
+          window.location = result.redirect;
+        }
+      
+        if(result.error) {
+          handleError(result.error);
+        }
+    
+        if(handler){
+            handler(result); 
+        }
+      };  
+    
