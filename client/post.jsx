d43520bc911d4f191d71e1c2b38885ae3b067e97
lailaporter55 <lmp3003@rit.edu>
2025-11-29 09:36:18 -0500

    post

---
+++ client/post.jsx
@@ -0,0 +1,9 @@
+/* similar to domomaker maker.jsx
+* users can post photos and reviews of their mahjong parties
+* will show up after the user logs into the party page
+*/
+
+const helper = require('./helper.js');
+const React = require('react'); 
+const { useState, useEffect } = React; 
+const { createRoot } = require('react-dom/client'); 
