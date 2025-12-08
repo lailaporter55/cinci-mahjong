6a73c6b2f26238c13aa1e262c89c72415ec393d4
lailaporter55 <lmp3003@rit.edu>
2025-12-08 11:28:30 -0500

    fixing index

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,2 +1,4 @@
 module.exports.Account = require('./Account.js');
 module.exports.Party = require('./Party.js');
+module.exports.Post = require('./Post.js');
+module.exports.Tiles = require('./Tiles.js');
